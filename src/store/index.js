1a0e15183903b5f69345fe6f045b4d630431ed93
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-11-21 15:48:58 +0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from './slices/counterSlice'
+import authSlice from './slices/authSlice'
+
 
 const store = configureStore({
     reducer: {
       counterSlice,
+        authSlice
     },
   });
   
